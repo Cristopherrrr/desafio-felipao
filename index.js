4876a4b5aac7a27f632725e4a28bbe82c7b087a7
Cristopherrrr <cristopherr.contato@gmail.com>
2025-09-17 19:58:06 -0300

    Desafio feito

---
+++ index.js
@@ -0,0 +1,26 @@
+// Atribuição de variáveis
+let nomeDoHeroi = "Cristopher";
+let xpDoHeroi = 10500;
+let nivel;
+
+// Estrutura de decisão para determinar o nível usei o laço de repetição
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivel = "Imortal";
+} else { // Este 'else' para finalizar o laço de repetição.
+    nivel = "Radiante";
+}
+
+// Saída final
+console.log(`O Herói de nome **${nomeDoHeroi}** está no nível de **${nivel}**`);
